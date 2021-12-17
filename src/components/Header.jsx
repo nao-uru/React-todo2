250921d8f84016dbbbd6028629062b3e628ebf13
nao <nao.uru0419@gmail.com>
2021-12-17 12:25:59 +0900

    Headerメッセージを初回のみの変更に

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { HeaderBack } from "../css/other"
 import { HeaderTime } from "./header-component/Headertime"
 import { HeaderMessage } from "./header-component/HeaderMessage"
+import { useMemo } from "react";
 
 export const Header = () => {
+
+  const message = useMemo(()=> HeaderMessage(),[]);
+
   return (
     <HeaderBack className="md:h-60 h-40 font-bold font-sans">
       <h1 className="sm:text-4xl text-2xl">HELLO!</h1>
-      <HeaderMessage/>
+      {message}
       <p className="sm:text-2xl text-xl mt-2 sm:mt-4">DATE  :{HeaderTime()}</p>
     </HeaderBack>
   )
